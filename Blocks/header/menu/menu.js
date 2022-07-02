eb20282d4be80cd8ac7093987f0e2bbb6b58db81
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-02 16:39:27 -0300

    Feature: Form structure
    Implementando o CSS do formulário, precisa ainda de uns ajustes de espaçamento.

--- Blocks/header/menu/menu.js
+++ Blocks/header/menu/menu.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".menu");
 const menuTwo = document.querySelectorAll(".header__nav_item")[1];
 
 function handleMenu() {
-  menu.classList.toggle("menu__opened");
+  menu.classList.toggle("menu_opened");
 }
 
 menuTwo.addEventListener("click", () => {
--- Page/script.js
+++ Page/script.js
@@ -4,7 +4,7 @@ const menu = document.querySelector(".menu");
 const menuTwo = document.querySelectorAll(".header__nav_item")[1];
 
 function handleMenu() {
-  menu.classList.toggle("menu__opened");
+  menu.classList.toggle("menu_opened");
 }
 
 menuTwo.addEventListener("click", () => {
@@ -31,10 +31,69 @@ const jobs = [
 ];
 
 const selectJob = document.querySelector('.select-job');
+const allForm = document.querySelector('hero-form');
+const clearForm = () => { allForm.innerHTML = ''; };
+const website = document.querySelectorAll('.website');
+const allInput = document.querySelectorAll('.form__input');
+const submit = document.querySelector('#submit-btn');
 
 function addOptionsToSelectJobs(jobs) {
-  return jobs.forEach((job, key) => selectJob[key] = new Option(job,key));
+  return jobs.forEach((job, key) => selectJob[key+1] = new Option(job,key+1));
 }
 
 selectJob.addEventListener('click', addOptionsToSelectJobs(jobs));
 
+let inputWebsite = '';
+let inputJob = '';
+
+function checkJobInput() {
+  selectJob.forEach((input) => {
+    input.addEventListener('click', (event) => {
+      selectJob = event.target.value;
+    });
+  });
+}
+
+function checkFormSubmit() {
+  return allInput.forEach((input) => {
+    if (input.value !== '') {
+      submit.disabled = false;
+    } else {
+      submit.disabled = true;
+    }
+  });
+}
+
+let formAnswers = [];
+
+function saveFormInformation() {
+  const currentFormAnswers = {
+    name: document.querySelector('.input-name').value,
+    email: document.qduerySelector('.input-email').value,
+    phone: document.querySelector('.input-phone').value,
+    job: checkJobInput(),
+    password: document.querySelector('.input-password').value,
+    website:checkJobInput(),
+  }
+  return formAnswers.push(currentFormAnswers);
+}
+
+function submitFormAnswer() {
+  const endMessage = "Obrigado!, entraremos em contato"
+  allForm.innerHTML = endMessage;
+}
+
+function onFormSubmit() {
+  checkFormSubmit();
+  saveFormInformation();
+  clearForm();
+  submitFormAnswer();
+}
+
+// submit.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   checkFormSubmit();
+//   saveFormInformation();
+//   clearForm();
+//   submitFormAnswer();
+// });
