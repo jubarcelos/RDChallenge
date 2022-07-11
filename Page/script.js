e5454240c7805d881c05cee329b70da1dd21b9a7
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-07-11 09:53:11 -0300

    feature: buton header media
    implementa lógica de troca de ícone, aplica css, mas não consegui terminar de ajusatar o menu.

--- Page/script.js
+++ Page/script.js
@@ -3,13 +3,53 @@
 const menu = document.querySelector(".menu");
 const menuTwo = document.querySelectorAll(".header__nav_item")[1];
 
+const sanduicheIcon = document.querySelector('.sanduiche');
+const sanduicheMenu  = document.querySelector('.menu__sanduiche');
+const sanduicheBox = document.querySelector('.sanduiche__container');
+const navMenu = document.querySelector('.header__nav');
+const closeBtn = document.querySelector('.sanduiche_close-btn');
+
+function changeToExitIcon() {
+  sanduicheIcon.style.display = 'none';
+  closeBtn.style.display = 'flex';
+}
+
+function changeToSanduicheIcon() {
+  sanduicheIcon.style.display = 'flex';
+  closeBtn.style.display = 'none';
+}
+
+function openSanduicheMenu() {
+  sanduicheMenu.style.display = 'flex';
+  closeBtn.style.display = 'flex';
+  sanduicheBox.style.display = "flex";
+  navMenu.style.display = "flex";
+}
+
+function closeSanduicheMenu() {
+  sanduicheMenu.style.display = 'none';
+  closeBtn.style.display = 'none';
+  sanduicheBox.style.display = "none";
+  navMenu.style.display = "none";
+}
+
+sanduicheIcon.addEventListener('click', () =>{ 
+  changeToExitIcon();
+  openSanduicheMenu();
+});
+
+closeBtn.addEventListener('click', () =>{ 
+  closeSanduicheMenu();
+  changeToSanduicheIcon();
+});
+
 function handleMenu() {
   menu.classList.toggle("menu_opened");
 }
 
-menuTwo.addEventListener("click", function() {
-  handleMenu();
-});
+menuTwo.addEventListener("click", () => handleMenu());
+
+
 
 
 // FORM input select options
@@ -118,34 +158,3 @@ dotsNav.addEventListener('click', event => {
   currentDot.classList.remove('.current-slide');
   targetDot.classList.add('.current-slide');
 })
-
-
-// const currentSlide = track.querySelector('.current-slide');
-// const currentDot = dotsNav.querySelector('.current-slide');
-
-// function moveToSlide(track, currentSlide, targetSlide) {
-//   track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-//   updateSlides(currentSlide, targetSlide)
-// }
-
-// dotsNav.addEventListener('click', event => {
-//   const targetDot = event.target.closest('button');
-  
-//   if(!targetDot) return;
-  
-//   const targetIndex = dots.findIndex(dot => dot === targetDot);
-//   const targetSlide = slides[targetIndex];
-  
-//   moveToSlide(track, currentSlide, targetSlide)
-//   updateDots(currentDot, targetDot)
-// })
-
-// function updateSlides(currentSlide, targetSlide){
-//   currentSlide.classList.remove('.current-slide');
-//   targetSlide.classList.add('.current-slide');
-// }
-
-// function updateDots(currentDot, targetDot) {
-//   currentDot.classList.remove('.current-slide');
-//   targetDot.classList.add('.current-slide');
-// }
